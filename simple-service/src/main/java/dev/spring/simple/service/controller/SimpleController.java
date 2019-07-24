d42f56acc7085eeac989ecfc641e3130634c522a
bogdanmic <mic.bogdan@gmail.com>
2019-07-25 01:16:25 +0300

    Expose the data from the database into our test controller.

--- simple-service/src/main/java/dev/spring/simple/service/controller/SimpleController.java
+++ simple-service/src/main/java/dev/spring/simple/service/controller/SimpleController.java
@@ -1,25 +1,33 @@
 package dev.spring.simple.service.controller;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.spring.simple.service.aspect.LogPerformance;
+import dev.spring.simple.service.persistance.entity.ContactEntity;
+import dev.spring.simple.service.service.ContactService;
 
 @RestController
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class SimpleController {
+
+    private final ContactService contactService;
+
     @LogPerformance
     @GetMapping(value = "/api/v1/contacts")
-    public String getContacts() {
-        return String.format("Get contacts");
+    public List<ContactEntity> getContacts() {
+        return contactService.getContacts();
     }
 
     @LogPerformance
     @GetMapping(value = "/api/v1/contacts/{contactId}")
-    public String getContact(@PathVariable(name = "contactId") Long contactId) {
-        return String.format("Get contact \'%s\'.", contactId);
+    public ContactEntity getContact(@PathVariable(name = "contactId") Long contactId) {
+        return contactService.getContact(contactId);
     }
 }
